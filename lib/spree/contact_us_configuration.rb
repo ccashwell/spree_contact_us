fa5354fd05e09fb3cae8d55e8a248a26936b645f
Chris Cashwell <ccashwell@gmail.com>
2015-07-27 12:25:28 -0500

    Add options for subjects

--- lib/spree/contact_us_configuration.rb
+++ lib/spree/contact_us_configuration.rb
@@ -1,3 +1,9 @@
 class Spree::ContactUsConfiguration < Spree::Preferences::Configuration
   preference :contact_tracking_message, :string, :default => nil
+  preference :contact_subject_options, :array, :default => [
+    "I am a new customer and have a question",
+    "I have a question about a specific product",
+    "I am inquiring about a late order",
+    "I have a question about something else"
+  ]
 end
